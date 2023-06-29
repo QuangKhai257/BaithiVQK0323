0cbdeb563b1c7b0e2000415105bbb72879b2aff9
QuangKhai257 <khaibee257@gmail.com>
2023-06-29 08:48:46 +0700

    VQK

---
+++ BaithiVQK0323/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace BaithiVQK0323.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
